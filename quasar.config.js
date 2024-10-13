7fdf3abde5cb32b667984f1357d18c4538d5a855
swe-robertkibet <swe.robertkibet@gmail.com>
2024-10-13 10:25:46 +0300

    store counter and counter input to local storage for persistent storage

--- quasar.config.js
+++ quasar.config.js
@@ -21,8 +21,8 @@ module.exports = configure(function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
     boot: [
-      
-      
+
+
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
@@ -47,7 +47,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
       target: {
-        browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
+        browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node20'
       },
 
@@ -70,7 +70,7 @@ module.exports = configure(function (/* ctx */) {
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
 
-      
+
       // vitePlugins: [
       //   [ 'package-name', { ..pluginOptions.. }, { server: true, client: true } ]
       // ]
@@ -97,7 +97,7 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: ['LocalStorage']
     },
 
     // animations: 'all', // --- includes all animations
@@ -119,7 +119,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/developing-ssr/configuring-ssr
     ssr: {
       // ssrPwaHtmlFilename: 'offline.html', // do NOT use index.html as name!
-                                          // will mess up SSR
+      // will mess up SSR
 
       // extendSSRWebserverConf (esbuildConf) {},
       // extendPackageJson (json) {},
@@ -130,7 +130,7 @@ module.exports = configure(function (/* ctx */) {
       // manualPostHydrationTrigger: true,
 
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       middlewares: [
         'render' // keep this as last one
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -45,19 +45,44 @@
   margin: 0 auto;
 }
 </style>
-
 <script setup lang="ts">
-import { watch, reactive } from 'vue';
 
-const data = reactive({
+// Imports
+import { watch, reactive, onMounted } from 'vue';
+import { useQuasar } from 'quasar'
+
+// Quasar
+const $q = useQuasar()
+
+// Define the shape of the data
+interface SavedData {
+  counter: number;
+  name: string;
+}
+
+// Reactive data object
+const data = reactive<SavedData>({
   counter: 0,
   name: ''
 })
 
-watch(() => data.counter, (newValue) => {
-  console.log('Counter changed:', newValue)
+// Load saved data from localStorage when app starts
+onMounted(() => {
+  const savedData = $q.localStorage.getItem('data')
+
+  if (savedData && typeof savedData === 'object') {
+    // Ensure savedData has counter and name properties
+    data.counter = (savedData as SavedData).counter ?? 0
+    data.name = (savedData as SavedData).name ?? ''
+  }
 })
 
+// Watch for changes in the counter and name, and store them in localStorage
+watch([() => data.counter, () => data.name], () => {
+  $q.localStorage.set('data', { counter: data.counter, name: data.name })
+})
+
+// Counter Methods
 const increaseCounter = () => {
   data.counter++
 }
@@ -70,11 +95,13 @@ const resetCounter = () => {
   data.counter = 0
 }
 
-const handlePan = (e: { delta: { y: number } }) => {
-  if (e.delta.y < 0) {
+// Handle swipe up/down gesture to increase/decrease the counter
+const handlePan = (e: { delta?: { x?: number; y?: number } }) => {
+  if (e.delta?.y && e.delta.y < 0) {
     increaseCounter()
   } else {
     decreaseCounter()
   }
 }
+
 </script>
